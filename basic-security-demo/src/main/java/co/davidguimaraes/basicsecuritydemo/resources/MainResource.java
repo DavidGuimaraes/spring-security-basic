f2d88a7fbf922e06ba297eaaa64da2deb1e5a967
David Guimaraes <david.ce@live.com>
2020-04-02 19:27:35 -0400

    03. Testando suporte a arquivos PDF.

--- basic-security-demo/src/main/java/co/davidguimaraes/basicsecuritydemo/resources/MainResource.java
+++ basic-security-demo/src/main/java/co/davidguimaraes/basicsecuritydemo/resources/MainResource.java
@@ -1,10 +1,17 @@
 package co.davidguimaraes.basicsecuritydemo.resources;
 
 import co.davidguimaraes.basicsecuritydemo.models.Hello;
+import org.springframework.core.io.ByteArrayResource;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @RestController
 @RequestMapping("/api")
@@ -18,5 +25,17 @@ public class MainResource {
     @GetMapping(value = "/hello/xml", consumes = MediaType.ALL_VALUE, produces = MediaType.APPLICATION_XML_VALUE)
     public Hello hello2(){
         return new Hello(1L, "Hello!");
+    }
+
+    @GetMapping(value = "/hello/pdf", consumes = MediaType.ALL_VALUE, produces = MediaType.APPLICATION_PDF_VALUE)
+    public ResponseEntity<ByteArrayResource> hello3() throws IOException {
+
+        Path pdfPath = Paths.get("/absolute/path/to/file/file.pdf");
+        byte[] pdf = Files.readAllBytes(pdfPath);
+
+        return ResponseEntity
+                .ok()
+                .contentLength(pdf.length)
+                .body(new ByteArrayResource(pdf));
     }
 }
